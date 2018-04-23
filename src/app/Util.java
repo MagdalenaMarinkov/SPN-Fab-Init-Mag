2a1bd511416a13e1de590c613bf49b4c46edbb60
Yannik Inniger <yannik.inniger@siroop.ch>
2018-04-23 19:33:56 +0200

    Add util more util methods

--- src/app/Util.java
+++ src/app/Util.java
@@ -11,8 +11,20 @@ public class Util {
         return String.format("%" + length + "s", intermediateResult).replace(' ', '0');
     }
 
+    public static String convertToBinaryWithPadding(int value, int length) {
+        String binary = Integer.toBinaryString(value);
+        return String.format("%" + length + "s", binary).replace(' ', '0');
+    }
+
+
     public static String xOr(int a, int b) {
         return Integer.toBinaryString(a ^ b);
+    }
+
+    public static String xOr(String a, String b) {
+        int intA = Integer.parseInt(a, 2);
+        int intB = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(intA ^ intB);
     }
 
     public static String[] divideToBlocks(int size, String string) {
